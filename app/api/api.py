a5cedd035bbbc1e6bfc52938459f2fc18aaac3f3
techinnovadors <techinnovadors@gmail.com>
2025-06-07 04:39:30 +0530

    Code Clean up

--- app/api/api.py
+++ app/api/api.py
@@ -1,15 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints.financial_document_analysis import financial_document_analysis
 from app.api.endpoints.company_websearch import company_websearch
 
 api_router = APIRouter()
-
-api_router.include_router(
-    financial_document_analysis.router,
-    prefix="/fsa",
-    tags=["Financial Document Analysis"],
-)
 
 
 api_router.include_router(
